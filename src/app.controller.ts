b05c3e5046678333160ef33bad05e0c4c8da33e6
Dhananjay Manjare <dhananjay.manjare@docquity.com>
2024-05-20 14:37:52 +0530

    new project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,8 +3,10 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  getHello(): any {
-    return 'Hello World!';
-  }
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
 }
