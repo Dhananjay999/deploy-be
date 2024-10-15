b23fc5026154c533538a724fa4cc9de73b3a6d63
dhananjay manjare <dhananjay.manjare@docquity.com>
2024-10-15 15:59:15 +0530

    cedwc

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template, jsonify, request
 
 app = Flask(__name__)
-
+apiEndPoint = 'abc'
 # Basic route
 @app.route('/')
 def home():
@@ -15,40 +15,40 @@ def get_data():
         'status': 200
     }
     return jsonify(data)
-@app.route('/get/metatag', methods=['GET'])
+@app.route('/get/'+apiEndPoint, methods=['GET'])
 def metaTags():
     meta_tags = {
         "title": "Home Page",
         "description": "This is the home page of my Flask app",
         "keywords": "Flask, Python, Web Development",
     }
-    return render_template('index2.html', meta_tags=meta_tags)
+    return render_template('index.html', meta_tags=meta_tags)
 
-@app.route('/get/metatag2', methods=['GET'])
+@app.route('/get/'+apiEndPoint+'2', methods=['GET'])
 def metaTags2():
     meta_tags = {
         "title": "Home Page",
         "description": "This is the home page of my Flask app",
         "keywords": "Flask, Python, Web Development",
     }
-    return render_template('index.html', meta_tags=meta_tags)
-@app.route('/get/metatag3', methods=['GET'])
+    return render_template('index2.html', meta_tags=meta_tags)
+@app.route('/get/'+apiEndPoint+'3', methods=['GET'])
 def metaTags3():
     meta_tags = {
         "title": "Home Page",
         "description": "This is the home page of my Flask app",
         "keywords": "Flask, Python, Web Development",
     }
-    return render_template('index4.html', meta_tags=meta_tags)
-@app.route('/get/metatag4', methods=['GET'])
+    return render_template('index3.html', meta_tags=meta_tags)
+@app.route('/get/'+apiEndPoint+'4', methods=['GET'])
 def metaTags4():
     meta_tags = {
         "title": "Home Page",
         "description": "This is the home page of my Flask app",
         "keywords": "Flask, Python, Web Development",
     }
-    return render_template('index3.html', meta_tags=meta_tags)
-@app.route('/get/metatag5', methods=['GET'])
+    return render_template('index4.html', meta_tags=meta_tags)
+@app.route('/get/'+apiEndPoint+'5', methods=['GET'])
 def metaTags5():
     meta_tags = {
         "title": "Home Page",
